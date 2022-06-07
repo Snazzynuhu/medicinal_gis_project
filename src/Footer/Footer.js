41ec89a677306b40f0366d18c11051cb8e3048aa
HARUNA NUHU <harunanuhu17@gmail.com>
2022-06-07 14:51:20 +0100

    uploaded all images

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -1,12 +1,22 @@
-import React from 'react';
-import './Footer.css';
+import React from "react";
+import "./Footer.css";
 
 const Footer = () => {
+  const now = new Date().getFullYear();
   return (
     <footer>
-      <p>&copy; copyright all rights reserved developed by <span><a href='https://harunanuhu-portfolio.netlify.app/' target="_blank">haruna nuhu</a></span></p>
+      <p>
+        &copy; copyright  {now}. all rights reserved developed by{" "}
+        <span>
+          <a href="https://harunanuhu-portfolio.netlify.app/" target="_blank">
+            haruna nuhu
+          </a>
+        </span> 
+        <br/>
+        thesis for the award of the Postgraduate Diploma in RS/GIS(ARCSSTEE).
+      </p>
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
